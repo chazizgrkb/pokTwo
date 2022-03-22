da90859a1257cab47cf6d1269c43394cd39e96bf
Chaz "Gamerappa" Péloquin <chazpelo2@gmail.com>
2022-03-22 15:45:29 -0400

    Update get_still.php

--- get_still.php
+++ get_still.php
@@ -3,19 +3,30 @@ require('lib/common.php');
 
 $video = htmlspecialchars($_GET["video_id"]);
 
-switch ($_GET["still_id"]) {
-    case 1:
-        $file = "/thumbs/" . $video . ".1.jpg";
-		break;
-    case 2:
-        $file = "/thumbs/" . $video . ".2.jpg";
-		break;
-    case 3:
-        $file = "/thumbs/" . $video . ".3.jpg";
-        break;
-	default:
-		$file = "/img/thumbnail.jpg";
-		break;
+$thumb = "thumbs/" . $video . ".2.jpg";
+
+
+if(isset($_GET['still_id'])) {
+	switch ($_GET["still_id"]) {
+		case 1:
+			$thumb = "thumbs/" . $video . ".1.jpg";
+			break;
+		case 2:
+			$thumb = "thumbs/" . $video . ".2.jpg";
+			break;
+		case 3:
+			$thumb = "thumbs/" . $video . ".3.jpg";
+			break;
+		default:
+			$file = "img/thumbnail.jpg";
+			break;
+	}
+}
+
+if(file_exists($thumb)) {
+	$file = $thumb;
+} else {
+	$file = "/img/thumbnail.jpg";
 }
 
 header("Location:". $file);
