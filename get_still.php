9916a9d5ec283857aefcee90fadb491d4318e3ee
Chaz "Gamerappa" Péloquin <chazpelo2@gmail.com>
2022-03-22 15:38:52 -0400

    Update get_still.php

--- get_still.php
+++ get_still.php
@@ -1,6 +1,22 @@
 <?php
 require('lib/common.php');
-$file = "/img/thumbnail.jpg";
+
+$video = htmlspecialchars($_GET["video_id"]);
+
+switch ($_GET["still_id"]) {
+    case 1:
+        $file = "/thumbs/" . $video . ".1.jpg";
+		break;
+    case 2:
+        $file = "/thumbs/" . $video . ".2.jpg";
+		break;
+    case 3:
+        $file = "/thumbs/" . $video . ".3.jpg";
+        break;
+	default:
+		$file = "/img/thumbnail.jpg";
+		break;
+}
 
 header("Location:". $file);
 
