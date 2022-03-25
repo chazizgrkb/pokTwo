78e4cac10611fa2c4e69ec7c1df043db727b146f
Chaz "Gamerappa" Péloquin <chazpelo2@gmail.com>
2022-03-25 17:06:01 -0400

    Update videos_page.php

--- videos_page.php
+++ videos_page.php
@@ -3,7 +3,7 @@ require('lib/common.php');
 
 $twig = twigloader();
 
-$videoData = query("SELECT $userfields v.video_id, v.title, v.description, v.time, (SELECT COUNT(*) FROM views WHERE video_id = v.video_id) AS views, (SELECT COUNT(*) FROM comments WHERE id = v.video_id) AS comments, v.videolength, v.tags, v.category_id, v.author FROM videos v JOIN users u ON v.author = u.id ORDER BY v.video_id DESC LIMIT 5");
+$videoData = query("SELECT $userfields v.video_id, v.title, v.description, v.time, (SELECT COUNT(*) FROM views WHERE video_id = v.video_id) AS views, (SELECT COUNT(*) FROM comments WHERE id = v.video_id) AS comments, v.videolength, v.tags, v.category_id, v.author FROM videos v JOIN users u ON v.author = u.id ORDER BY v.time DESC LIMIT 5");
 
 $mostViewedData = query("SELECT $userfields v.video_id, v.title, v.description, v.time, (SELECT COUNT(*) FROM views WHERE video_id = v.video_id) AS views, (SELECT COUNT(*) FROM comments WHERE id = v.video_id) AS comments, v.videolength, v.tags, v.category_id, v.author FROM videos v JOIN users u ON v.author = u.id ORDER BY views DESC LIMIT 5");
 
