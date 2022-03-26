72ce3b4e669b0fab551167ca5fb9075f9c6f45d2
Chaz "Gamerappa" Péloquin <chazpelo2@gmail.com>
2022-03-25 22:45:16 -0400

    remove v.tags bullshit

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $showSmallHeader = true;
 
 $twig = twigloader();
 
-$videoData = query("SELECT $userfields v.video_id, v.title, v.description, v.time, (SELECT COUNT(*) FROM views WHERE video_id = v.video_id) AS views, (SELECT COUNT(*) FROM comments WHERE id = v.video_id) AS comments, v.videolength, v.tags, v.category_id, v.author FROM videos v JOIN users u ON v.author = u.id ORDER BY RAND() LIMIT 5");
+$videoData = query("SELECT $userfields v.video_id, v.title, v.description, v.time, (SELECT COUNT(*) FROM views WHERE video_id = v.video_id) AS views, (SELECT COUNT(*) FROM comments WHERE id = v.video_id) AS comments, v.videolength, v.category_id, v.author FROM videos v JOIN users u ON v.author = u.id ORDER BY RAND() LIMIT 5");
 
 $tagData = query("SELECT * from tag_meta ORDER BY RAND() LIMIT 50");
 
