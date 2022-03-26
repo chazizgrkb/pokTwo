2fafdc2592523d33e52e5d5ebddbf8ac586f6df6
Chaz "Gamerappa" Péloquin <chazpelo2@gmail.com>
2022-03-26 17:35:10 -0400

    Create remove_favorites.php

---
+++ remove_favorites.php
@@ -0,0 +1,19 @@
+<?php
+require ('lib/common.php');
+
+if (isset($_GET['video_id']))
+{
+    if (result("SELECT * from favorites WHERE video_id = ? AND user_id = ?", [$_GET['video_id'], $userdata['id']]))
+    {
+        query("DELETE FROM favorites WHERE video_id = ? AND user_id = ?", [$_GET['video_id'], $userdata['id']]);
+        echo "<script type='text/javascript'>window.close();</script>";
+    }
+    else
+    {
+        die("You've ethier already removed the video or haven't favorited it yet!");
+    }
+}
+else
+{
+    die("invalid");
+}
