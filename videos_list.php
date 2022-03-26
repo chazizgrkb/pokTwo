5f88749761eb8a9faba486cf7b3891f9a4c70e1f
Chaz "Gamerappa" Péloquin <chazpelo2@gmail.com>
2022-03-26 15:01:15 -0400

    Update videos_list.php

--- videos_list.php
+++ videos_list.php
@@ -14,25 +14,6 @@ if (!isset($userpagedata) || !$userpagedata) {
 	error('404', "No user specified.");
 }
 
-$page = (isset($_GET['page']) && is_numeric($_GET['page']) && $_GET['page'] > 0 ? $_GET['page'] : 1);
-$forceuser = isset($_GET['forceuser']);
-
-if (isset($userpagedata['birthday'])) {
-$date = new DateTime($userpagedata['birthday']); // YYYY-MM-DD
-$now = new DateTime();
-$interval = $now->diff($date);
-$age = $interval->y;
-} else {
-$age = false;
-}
-
-// Personal user page stuff
-if ($userpagedata['about']) {
-	$markdown = new Parsedown();
-	$markdown->setSafeMode(true);
-	$userpagedata['about'] = $markdown->text($userpagedata['about']);
-}
-
 $twig = twigloader();
 echo $twig->render('videos_list.twig', [
 	'id' => $userpagedata['id'],
@@ -40,11 +21,4 @@ echo $twig->render('videos_list.twig', [
 	'latestVideo' => $latestVideo,
 	'allVideos' => $allVideos,
 	'userpagedata' => $userpagedata,
-	'forceuser' => $forceuser,
-	'page' => $page,
-	'edited' => (isset($_GET['edited']) ? true : false), // TODO: merge these three stuffs into one variable
-	'justbanned' => (isset($_GET['justbanned']) ? $_GET['justbanned'] : null),
-	'age' => $age,
-	'relationship' => relationship_to_type($userpagedata['relationshipStatus']),
-	'gender' => gender_to_type($userpagedata['gender']),
 ]);
