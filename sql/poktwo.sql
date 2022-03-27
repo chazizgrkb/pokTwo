992be33ec1ddcef87ba4599ec3c794d94ec2939f
Chaz "Gamerappa" Péloquin <chazpelo2@gmail.com>
2022-03-27 13:06:52 -0400

    better tags shit

--- sql/poktwo.sql
+++ sql/poktwo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 26, 2022 at 10:28 PM
+-- Generation Time: Mar 27, 2022 at 07:05 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
