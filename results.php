3332629cedab6f7809592f98970fa8da445ee59d
Chaz "Gamerappa" Péloquin <chazpelo2@gmail.com>
2022-03-29 17:09:27 -0400

    why the fuc is pagination broken

--- results.php
+++ results.php
@@ -2,13 +2,18 @@
 require('lib/common.php');
 
 $query = isset($_GET['search']) ? $_GET['search'] : null;
+$page = (isset($_GET['page']) && is_numeric($_GET['page']) && $_GET['page'] > 0 ? $_GET['page'] : 1);
 
 // currently selects all uploaded videos
 $videoData = query("SELECT $userfields v.video_id, v.title, v.description, v.time, (SELECT COUNT(*) FROM views WHERE video_id = v.video_id) AS views, (SELECT COUNT(*) FROM comments WHERE id = v.video_id) AS comments, (SELECT COUNT(*) FROM favorites WHERE video_id = v.video_id) AS favorites, v.author FROM videos v JOIN users u ON v.author = u.id WHERE v.title LIKE CONCAT('%', ?, '%') OR v.description LIKE CONCAT('%', ?, '%') ORDER BY v.id DESC", [$query, $query]);
 
+$count = result("SELECT COUNT(*) FROM videos v WHERE v.title LIKE CONCAT('%', ?, '%') OR v.description LIKE CONCAT('%', ?, '%') ORDER BY v.id DESC", [$query, $query]);
+
 $twig = twigloader();
 
 echo $twig->render('results.twig', [
-	'videos' => $videoData,
-	'query' => $query
+	'videos' => fetchArray($videoData),
+	'query' => $query,
+	'page' => $page,
+	'count' => $count
 ]);
