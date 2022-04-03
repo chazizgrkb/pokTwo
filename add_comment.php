cb7ecaba792e27eb45f919748cff075fa3ce5477
Chaz "Gamerappa" Péloquin <chazpelo2@gmail.com>
2022-04-03 17:29:41 -0400

    fix comments being broken on macOS Production Server

--- add_comment.php
+++ add_comment.php
@@ -4,8 +4,7 @@ require('lib/common.php');
 
 if (isset($_POST['video_id']))
 {
-	$reply_to = (isset($_POST['reply_to']) ? $_POST['reply_to'] : "");
-	$comments->addComment($_POST['video_id'],$reply_to,$_POST['comment'],$userdata['id']);
+	$comments->addComment($_POST['video_id'],$_POST['comment'],$userdata['id']);
 	closeWindow();
 } else {
 die("invalid");
--- lib/videoComments.php
+++ lib/videoComments.php
@@ -1,9 +1,10 @@
 <?php
 namespace pokTwo;
 class Comments extends Videos {
-	function addComment($videoID, $reply_to, $comment, $id) {
+	function addComment($videoID, $comment, $id) {
 		global $mysql;
-		$mysql->query("INSERT INTO comments (id, reply_to, comment, author, date, deleted) VALUES (?,?,?,?,?,?)",
+		$mysql->query("INSERT INTO comments (id, comment, author, date, deleted) VALUES (?,?,?,?,?,?)",
 		[$videoID, $reply_to, $comment, $id, time(), 0]);
+		//[$videoID, $reply_to, $comment, $id, time(), 0]);
 	}
 }
