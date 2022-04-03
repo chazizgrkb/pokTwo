bda9b4bd9d6132045ca021e95ee5edce5c4a702d
Chaz "Gamerappa" Péloquin <chazpelo2@gmail.com>
2022-04-03 17:30:24 -0400

    Update videoComments.php

--- lib/videoComments.php
+++ lib/videoComments.php
@@ -3,8 +3,8 @@ namespace pokTwo;
 class Comments extends Videos {
 	function addComment($videoID, $comment, $id) {
 		global $mysql;
-		$mysql->query("INSERT INTO comments (id, comment, author, date, deleted) VALUES (?,?,?,?,?,?)",
-		[$videoID, $reply_to, $comment, $id, time(), 0]);
+		$mysql->query("INSERT INTO comments (id, comment, author, date, deleted) VALUES (?,?,?,?,?)",
+		[$videoID, $comment, $id, time(), 0]);
 		//[$videoID, $reply_to, $comment, $id, time(), 0]);
 	}
 }
