4938dc3c12c9c57ffd563e37dceec1ec601bdbce
forkiesassds <icanttellyou@memeware.net>
2022-04-08 22:36:32 +0300

    fix unfavoriting not working

--- remove_favorites.php
+++ remove_favorites.php
@@ -1,12 +1,12 @@
 <?php
 namespace pokTwo;
 require ('lib/common.php');
-
+global $sql;
 if (isset($_GET['video_id']))
 {
-    if (result("SELECT * from favorites WHERE video_id = ? AND user_id = ?", [$_GET['video_id'], $userdata['id']]))
+    if ($sql->result("SELECT * from favorites WHERE video_id = ? AND user_id = ?", [$_GET['video_id'], $userdata['id']]))
     {
-        query("DELETE FROM favorites WHERE video_id = ? AND user_id = ?", [$_GET['video_id'], $userdata['id']]);
+        $sql->query("DELETE FROM favorites WHERE video_id = ? AND user_id = ?", [$_GET['video_id'], $userdata['id']]);
         echo "<script type='text/javascript'>window.close();</script>";
     }
     else
