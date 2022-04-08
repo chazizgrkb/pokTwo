be29c7c07d9914584b116c90f2cacda34053c626
forkiesassds <icanttellyou@memeware.net>
2022-04-08 23:02:43 +0300

    add pagination to browse

--- browse.php
+++ browse.php
@@ -3,26 +3,32 @@ namespace pokTwo;
 require('lib/common.php');
 
 $section = (isset($_GET['s']) ? $_GET['s'] : null);
+$page = (isset($_GET['page']) && is_numeric($_GET['page']) && $_GET['page'] > 0 ? $_GET['page'] : 1);
+$limit = sprintf("%s,%s", (($page - 1) * $lpp), $lpp);
 
 if ($section == "mp") {
-$videoData = Videos::getVideos("views DESC", 20);
+$videoData = Videos::getVideos("views DESC", $limit);
 $pageName = "browsePopular";
 } elseif ($section == "md") {
-$videoData = Videos::getVideos("comments DESC", 20);
+$videoData = Videos::getVideos("comments DESC", $limit);
 $pageName = "browseDiscussed";
 } elseif ($section == "mf") {
-$videoData = Videos::getVideos("favorites DESC", 20);
+$videoData = Videos::getVideos("favorites DESC", $limit);
 $pageName = "browseFavorited";
 } elseif ($section == "r") {
-$videoData = Videos::getVideos("RAND()", 20);
+$videoData = Videos::getVideos("RAND()", $limit);
 $pageName = "browseRandom";
 } else {
-$videoData = Videos::getVideos("v.time DESC", 20);
+$videoData = Videos::getVideos("v.time DESC", $limit);
 $pageName = "browseMain";
 }
+
+$count = $sql->result("SELECT COUNT(*) FROM videos v");
 
 $twig = twigloader();
 
 echo $twig->render('browse.twig', [
 	'videos' => $videoData,
+	'page' => $page,
+	'level_count' => $count
 ]);
