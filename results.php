2a67353abda78481a5299f3c049ac2787ebdd419
forkiesassds <icanttellyou@memeware.net>
2022-04-08 21:39:54 +0300

    show search results for tags

--- results.php
+++ results.php
@@ -6,9 +6,30 @@ $query = isset($_GET['search']) ? $_GET['search'] : null;
 $page = (isset($_GET['page']) && is_numeric($_GET['page']) && $_GET['page'] > 0 ? $_GET['page'] : 1);
 
 // currently selects all uploaded videos
-$videoData = $sql->query("SELECT $userfields $videofields FROM videos v JOIN users u ON v.author = u.id WHERE v.title LIKE CONCAT('%', ?, '%') OR v.description LIKE CONCAT('%', ?, '%') ORDER BY v.id DESC", [$query, $query]);
+$videoData = $sql->query("
+SELECT DISTINCT $userfields $videofields FROM videos v 
+	JOIN users u ON v.author = u.id 
+	JOIN tag_index ti ON (ti.video_id = v.id) 
+	JOIN tag_meta t ON (t.tag_id = ti.tag_id) 	
+	WHERE 
+		v.title LIKE CONCAT('%', ?, '%') 
+	OR 
+		v.description LIKE CONCAT('%', ?, '%') 
+	OR
+		t.name LIKE CONCAT('%', ?, '%') 
+ORDER BY v.id DESC", [$query, $query, $query]);
 
-$count = $sql->result("SELECT COUNT(*) FROM videos v WHERE v.title LIKE CONCAT('%', ?, '%') OR v.description LIKE CONCAT('%', ?, '%') ORDER BY v.id DESC", [$query, $query]);
+$count = $sql->result("
+SELECT COUNT(DISTINCT v.id) FROM videos v
+ 	JOIN tag_index ti ON (ti.video_id = v.id) 
+	JOIN tag_meta t ON (t.tag_id = ti.tag_id)
+	WHERE 
+		v.title LIKE CONCAT('%', ?, '%') 
+	OR v.description 
+		LIKE CONCAT('%', ?, '%') 
+	OR
+		t.name LIKE CONCAT('%', ?, '%') 
+ORDER BY v.id DESC", [$query, $query, $query]);
 
 $twig = twigloader();
 
