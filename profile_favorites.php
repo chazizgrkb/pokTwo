78b2b5da4a185d14b3591be486bb97c999403562
forkiesassds <icanttellyou@memeware.net>
2022-04-10 16:10:30 +0300

    make profile_favorites.php show favorites, and add pagination to both of user pages

--- profile_favorites.php
+++ profile_favorites.php
@@ -4,6 +4,8 @@ namespace pokTwo;
 require('lib/common.php');
 
 $pageName = "profile_favorites";
+$page = (isset($_GET['page']) && is_numeric($_GET['page']) && $_GET['page'] > 0 ? $_GET['page'] : 1);
+$limit = sprintf("LIMIT %s,%s", (($page - 1) * $lpp), $lpp);
 
 if (isset($_GET['id'])) {
     $userpagedata = $sql->fetch("SELECT * FROM users WHERE id = ?", [$_GET['id']]);
@@ -16,7 +18,17 @@ $favoritesCount = $sql->result("SELECT COUNT(user_id) FROM favorites WHERE user_
 
 $twig = twigloader();
 
-$videos = Videos::getVideos('v.time DESC', 5, 'v.author', $userpagedata['id']);
+$videoData = $sql->query("
+SELECT $userfields $videofields FROM videos v 
+	JOIN users u ON v.author = u.id 
+	JOIN favorites f ON (f.video_id = v.video_id) 
+	WHERE 
+		f.user_id = ?
+ORDER BY v.time DESC $limit", [$userpagedata['id']]);
+$videos = $sql->fetchArray($videoData);
+foreach ($videos as &$video) {
+    $video['tags'] = VideoTags::getVideoTags($video['id']);
+}
 
 echo $twig->render('profile_videos.twig', [
     'id' => $userpagedata['id'],
@@ -25,4 +37,7 @@ echo $twig->render('profile_videos.twig', [
     'allFavorites' => $favoritesCount,
     'userpagedata' => $userpagedata,
     'videos' => $videos,
+	'page' => $page,
+    'level_count' => $favoritesCount,
+	'page_name' => $pageName
 ]);
--- profile_videos.php
+++ profile_videos.php
@@ -4,6 +4,8 @@ namespace pokTwo;
 require('lib/common.php');
 
 $pageName = "profile_videos";
+$page = (isset($_GET['page']) && is_numeric($_GET['page']) && $_GET['page'] > 0 ? $_GET['page'] : 1);
+$limit = sprintf("%s,%s", (($page - 1) * 5), 5);
 
 if (isset($_GET['id'])) {
     $userpagedata = $sql->fetch("SELECT * FROM users WHERE id = ?", [$_GET['id']]);
@@ -16,7 +18,7 @@ $favoritesCount = $sql->result("SELECT COUNT(user_id) FROM favorites WHERE user_
 
 $twig = twigloader();
 
-$videos = Videos::getVideos('v.time DESC', 5, 'v.author', $userpagedata['id']);
+$videos = Videos::getVideos('v.time DESC', $limit, 'v.author', $userpagedata['id']);
 
 echo $twig->render('profile_videos.twig', [
     'id' => $userpagedata['id'],
@@ -25,4 +27,7 @@ echo $twig->render('profile_videos.twig', [
     'allFavorites' => $favoritesCount,
     'userpagedata' => $userpagedata,
     'videos' => $videos,
+	'page' => $page,
+    'level_count' => $allVideos,
+	'page_name' => $pageName
 ]);
