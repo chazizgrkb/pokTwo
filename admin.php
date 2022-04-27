4219e68aa1d10afc5f7bbc7c83dfa02af4606219
forkiesassds <icanttellyou@memeware.net>
2022-04-27 17:29:05 +0300

    add comment section to admin panel and allow deletion of comments from there.

--- admin.php
+++ admin.php
@@ -5,9 +5,24 @@ require('lib/common.php');
 
 if ($userdata['powerlevel'] < 3) error('403', "You shouldn't be here, get out!");
 
+if(isset($_GET['action'])) {
+	if($_GET['action'] == 'delete_comment') {
+		if(isset($_GET['id'])) {
+			$sql->query("DELETE FROM comments WHERE comment_id = ?", [$_GET['id']]);
+		} else {
+			error('400', 'you forgot the id.');
+		}
+	}
+}
+
 $latestRegisteredUsers = $sql->query("SELECT id, name, joined FROM users ORDER BY joined DESC LIMIT 15");
 $latestSeenUsers = $sql->query("SELECT id, name, lastview FROM users ORDER BY lastview DESC LIMIT 15");
 $videoData = $sql->query("SELECT $userfields $videofields FROM videos v JOIN users u ON v.author = u.id ORDER BY v.time DESC LIMIT 7");
+$comments = $sql->fetchArray($sql->query("SELECT $userfields c.comment_id, c.id, c.comment, c.author, c.date, c.deleted, (SELECT COUNT(reply_to) FROM comments WHERE reply_to = c.comment_id) AS replycount FROM comments c JOIN users u ON c.author = u.id ORDER BY c.date DESC"));
+foreach ($comments as &$comment) {
+    $comment['allVideos'] = Users::getUserVideoCount($comment['author']);
+    $comment['allFavorites'] = Users::getUserFavoriteCount($comment['author']);
+}
 
 $thingsToCount = ['comments', 'users', 'videos', 'views', 'messages', 'favorites'];
 
@@ -25,4 +40,5 @@ echo $twig->render('admin.twig', [
     'things_to_count' => $thingsToCount,
     'count' => $count,
     'videos' => $videoData,
+	'comments' => $comments
 ]);
