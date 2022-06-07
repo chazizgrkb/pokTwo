f0d9271884517b39e6623e252d27ee0578979b67
Chaz "Gamerappa" Péloquin <chazpelo2@gmail.com>
2022-06-06 20:42:14 -0400

    o fuck

--- watch.php
+++ watch.php
@@ -11,39 +11,6 @@ $videoData = Videos::getVideoData($userfields, $id);
 
 if (!$videoData) playerError("This video does not exist.");
 
-/* $fmt_stream_map = [
-	[
-    "sig" => "44AA40D8A104309AE6E672CB78343D5F99ADA807.9ACF6ABD0873E6AA01C34B5489E53C0EF139D534",
-	"fallback_host" => "fuckyou.google.com",
-    "quality" => "small",
-	"itag" => "133",
-    "url" => 'media/' . $_GET['v'] . '.mp4',
-    "type" => "video/mp4; codecs=\"avc1.4d002a\""
-	],
-	[
-    "sig" => "44AA40D8A104309AE6E672CB78343D5F99ADA807.9ACF6ABD0873E6AA01C34B5489E53C0EF139D534",
-	"fallback_host" => "fuckyou.google.com",
-    "quality" => "small",
-	"itag" => "5",
-    "url" => 'media/' . $_GET['v'] . '.flv',
-    "type" => "video/x-flv;"
-	],
-];
-$count = 0;
-$url_encoded_fmt_stream_map;
-foreach($fmt_stream_map as $stream) {
-    if($count == 0) {
-        $url_encoded_fmt_stream_map = http_build_query($stream);
-    } else {
-        $url_encoded_fmt_stream_map = $url_encoded_fmt_stream_map . "," . http_build_query($stream);
-	}
-    $count++;
-}
-
-$adaptive_fmts = 'type%3Dvideo%252Fmp4%2Bcodecs%3D%2522avc1.4d401e%2522%26index%3D708-1471%26itag%3D133%26bitrate%3D2208158%26size%3D320x240%26url%3Dmedia%252F' . $_GET['v'] . '.mp4';
-
-$url_encoded_fmt_stream_map = str_replace("&","\u0026",$url_encoded_fmt_stream_map); */
-
 $commentData = VideoComments::getComments($id);
 
 $pageName = "watch";
@@ -69,17 +36,11 @@ if ($log) {
     } else {
         $isVideoOwnedByCurrentUser = false;
     }
+	$subscribed = result("SELECT COUNT(user) FROM subscriptions WHERE id=? AND user=?", [$userdata['id'], $videoData['author']]);
 } else {
     $isFavorited = false;
 	$isVideoOwnedByCurrentUser = false;
-}
-
-if (isset($_COOKIE['useFlashPlayer'])) {
-    $isFlash = $_COOKIE['useFlashPlayer'];
-} elseif ($_GET['flash'] ?? null) {
-    $isFlash = true;
-} else {
-    $isFlash = false;
+	$subscribed = 0;
 }
 
 $previousRecentView = $sql->result("SELECT most_recent_view from videos WHERE video_id = ?", [$id]);
@@ -91,10 +52,8 @@ echo $twig->render('watch.twig', [
     'video' => $videoData,
     'recentView' => $previousRecentView,
     'allVideos' => $allVideos,
-    'isFlash' => $isFlash,
     'recommended' => Videos::getRecommended($videoData['video_id']),
     'isFavorited' => $isFavorited,
-/* 	'url_encoded_fmt_stream_map' => $url_encoded_fmt_stream_map,
-	'adaptive_fmts' => $adaptive_fmts, */
 	'videoOwnershipFuckery' => $isVideoOwnedByCurrentUser,
+	'subscribed' => $subscribed,
 ]);
